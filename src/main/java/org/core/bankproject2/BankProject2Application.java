aadb839f692547e463d6e72ae1e218978f43219b
Olesia Kokhman <olesia.kokhman.pz.2021@lpnu.ua>
2024-05-02 18:18:09 +0300

    first commit

---
+++ src/main/java/org/core/bankproject2/BankProject2Application.java
@@ -0,0 +1,13 @@
+package org.core.bankproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankProject2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankProject2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/core/bankproject2/BankProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.core.bankproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankProject2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

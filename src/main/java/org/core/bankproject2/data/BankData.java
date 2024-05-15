2e109b6c740f0c99eb504d44522f48a1991ccf3e
Olesia Kokhman <olesia.kokhman.pz.2021@lpnu.ua>
2024-05-15 22:56:30 +0300

    fix bug

--- src/main/java/org/core/bankproject2/data/BankData.java
+++ src/main/java/org/core/bankproject2/data/BankData.java
@@ -1,9 +1,13 @@
 package org.core.bankproject2.data;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "banks")
+@Getter
+@Setter
 public class BankData {
 
     @Id
